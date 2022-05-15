e642931fe54abc4bd6b4c7fbfbfe963e7658688c
mdnnyercsn <mdnnyercsn@gmail.com>
2022-05-15 09:36:19 +0700

    Create Home Layout

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const expressLayouts = require("express-ejs-layouts");
+
+const app = express();
+
+// static files
+app.use(express.static("public"));
+app.use("/css", express.static(__dirname + "public/css"));
+
+// set view engine
+app.set("view engine", "ejs");
+app.use(expressLayouts);
+app.set("layout", "./layouts/homeLayout");
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.listen(3004, function() {
+  console.log("server is running");
+});
